f3bc4e1c62aadef728dc95811dc7fb7d78be7348
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-05 09:20:40 +0000

    Create edit post view

---
+++ resources/views/edit-post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Edit Post</h1>
+    <form action="/edit-post/{{$post->id}}" method="POST">
+        @csrf
+        @method('PUT')
+        <input type="text" name="title" name="title" value="{{$post->title}}">
+        <textarea name="body">{{$post->body}}</textarea>
+        <button>Save changes</button>
+    </form>
+</body>
+</html>
