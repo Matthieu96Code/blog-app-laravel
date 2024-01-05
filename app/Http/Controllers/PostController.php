e604df2bf18ac193b3d1498114c9c4f49786d906
Matthieu96Code <senoumatthieu96@gmail.com>
2024-01-05 09:19:59 +0000

    Create post controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function deletePost(Post $post) {
+        if (auth()->user()->id === $post['user_id']) {
+            $post->delete();
+        }
+        return redirect('/');
+    }
+
+    public function actuallyUpdatePost(Post $post, Request $request) {
+        if (auth()->user()->id !== $post['user_id']) {
+            return redirect('/');
+        }
+
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+        return redirect('/');
+    }
+
+    public function showEditScreen(Post $post) {
+        if (auth()->user()->id !== $post['user_id']) {
+            return redirect('/');
+        }
+
+        return view('edit-post', ['post' => $post]);
+    }
+
+    public function createPost(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+        Post::create($incomingFields);
+        return redirect('/');
+    }
+}
